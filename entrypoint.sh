6f115cfb27b2b99598fa7dee4429e07d3b8ad8ee
Will Pimblett <will@wjdp.uk>
2021-01-11 08:12:15 +0000

    Remove leftover `ls` from testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 cd $GITHUB_WORKSPACE
-ls
+
 htmltest ${INPUT_PATH:+ $INPUT_PATH} \
   ${INPUT_CONFIG:+ -c $INPUT_CONFIG} \
   ${INPUT_LOG_LEVEL:+ -l $INPUT_LOG_LEVEL} \
