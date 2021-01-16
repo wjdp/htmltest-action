2808d9a92e3d07568dd013cf160da379b985d26c
Will Pimblett <will@wjdp.uk>
2021-01-16 20:32:31 +0000

    Echo version information

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 set -euxo pipefail
 cd $GITHUB_WORKSPACE
 
+# Echo version information
+htmltest --version
+
+# Run the test suite
 htmltest ${INPUT_PATH:+ $INPUT_PATH} \
   ${INPUT_CONFIG:+ -c $INPUT_CONFIG} \
   ${INPUT_LOG_LEVEL:+ -l $INPUT_LOG_LEVEL} \
